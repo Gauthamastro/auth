2eb205f724ac86c682073708e83e73f8eb3a554c
Gautham J <Gauthamastro@gmail.com>
2021-07-14 13:43:35 +0530

    Change the signing to ED25519

--- src/App.tsx
+++ src/App.tsx
@@ -1,82 +1,61 @@
 import React, {useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
-import {web3Accounts, web3Enable, web3FromSource} from "@polkadot/extension-dapp";
-
+import {mnemonicGenerate} from '@polkadot/util-crypto';
+import { Keyring } from '@polkadot/keyring';
 const {blake2AsHex} = require('@polkadot/util-crypto');
+const { u8aToHex } = require('@polkadot/util');
 
 
 function App() {
 
     useEffect(() => {
         const getExtensionAddress = async () => {
+            const keyring = new Keyring({ type: 'ed25519', ss58Format: 2 });
+        // generate a mnemonic with default params (we can pass the number
+        // of words required 12, 15, 18, 21 or 24, less than 12 words, while
+        // valid, is not supported since it is more-easily crackable)
+            const mnemonic = mnemonicGenerate();
+        // create & add the pair to the keyring with the type and some additional
+        // metadata specified
+            const pair = keyring.addFromUri(mnemonic, {name: 'first pair'}, 'ed25519');
+        // the pair has been added to our keyring
+            console.log(keyring.pairs.length, 'pairs available');
 
-            const extensions = await web3Enable('Polkadex');
-            if (extensions.length > 0) {
-                const allAccounts = await web3Accounts();
-                if (allAccounts.length > 0) {
-                    console.log(allAccounts[0].address)
-                    // Get the address of user: for this example, I am taking the first address
-                    return allAccounts[0];
-
-                } else {
-                    console.warn('Please create account in Polka extension first.')
-                    return ""
-                }
-            } else {
-                console.warn('Please install Polka Chrome extension first from https://polkadot.js.org/extension/')
-                return ""
-            }
+        // log the name & address (the latter encoded with the ss58Format)
+            console.log(pair.meta.name, 'has address', pair.address);
+            return pair
         }
         // This is how auth should work
         // For more documenatation check out: https://polkadot.js.org/docs/extension
         // Please install polkadotjs-extension and create an account in it before running this application
-        getExtensionAddress().then(account => {
-            if (account !== "") {
-                console.log("Users's Address: ", account.address)
+        getExtensionAddress().then(pair => {
+
+                console.log("Users's Address: ", pair.address)
                 // GET Request to path {domain of Openware}/api/v2/barong/identity/time to get a response like {"time":1623693678}
                 // we use the value of 'time' as nonce
                 // using a hardcoded nonce as example
                 let nonce = "1623693678";
-                let address = account.address;
+                let address = pair.address;
                 console.log("Nonce from Openfinex: ", nonce);
 
                 let message = "#" + address + "#" + nonce.toString();
                 console.log("Message to sign: ", message);
-
-                web3FromSource(account.meta.source)
-                    .then(injector => {
-                        const signRaw = injector?.signer?.signRaw;
-
-                        if (!!signRaw) {
-                            // after making sure that signRaw is defined
-                            // we can use it to sign our message
-                            const hash = blake2AsHex(message)
-                            console.log("Message Hash", hash);
-                            signRaw({
-                                address,
-                                data: hash,
-                                type: 'bytes'
-                            }).then((obj: any) => {
-                                console.log("Signature to send to Openfinex's barong: ", obj.signature)
-
-                                // Now do
-                                // POST /api/v2/barong/identity/sessions
-                                let payload = {
-                                    "nickname": address,
-                                    "algo": "SR25519", // NOTE: OPENWARE TEAM PLEASE NOTE THIS
-                                    "hash": "Blake2",
-                                    "nonce": nonce,
-                                    "signature": obj.signature,
-                                    "captcha_response": ""
-                                }
-                            })
-                        }
-                    })
-
-            } else {
-                console.log("Unable to get address")
-            }
+                const hash = blake2AsHex(message)
+                console.log("Message Hash", hash);
+                const signature = pair.sign(hash);
+                console.log("Signature: ",u8aToHex(signature))
+                // Now do
+                // POST /api/v2/barong/identity/sessions
+                let payload = {
+                    "nickname": address,
+                    "algo": "ED25519", // NOTE: OPENWARE TEAM PLEASE NOTE THIS
+                    "hash": "Blake2",
+                    "nonce": nonce,
+                    "signature": u8aToHex(signature),
+                    "captcha_response": ""
+                }
+                console.log("Payload for Barong", payload)
 
         })
     }, []);
